d5fb8486b705ae80a46b3dca5c7b3dac00e31945
Chloe McKnight <cmcknight@bluebeam.com>
2020-12-23 14:59:16 -0600

    add gdalwarp comment

--- create_vrt_then_tile.js
+++ create_vrt_then_tile.js
@@ -6,21 +6,22 @@ const width = 3024;
 const height = 2160;
 
 const BeginMapTilingProcessRequest = {
-  // documentid: "some-document-id",
-  // pageindex: 0,
-  // projectid: "3a7f76e9-770e-4ffa-92ee-fe0d01552e0e",
-  // pageid: "3f8e2ab5-53e0-4b7c-99e4-bd2c9f1f9100",
+  documentid: "some-document-id",
+  pageindex: 0,
+  projectid: "some-project-id",
+  pageid: "some-page-id",
   calibrationconfig: {
-    // scale: { from: { unit: "IN", value: 0.125 }, to: { unit: "FT", value: 1 } }, // used for recalibrating
-    // croppath: [
-    //   { x: 0.12204801647484315, y: 0.09078590793152891 },
-    //   { x: 0.060104541602532514, y: 0.31571815727373986 },
-    //   { x: 0.272067369787775, y: 0.6422764228605643 },
-    //   { x: 0.7898761052951476, y: 0.6436314364059776 },
-    //   { x: 0.8527874469953014, y: 0.44579945808058374 },
-    //   { x: 0.6592140878979672, y: 0.10840108409642901 },
-    //   { x: 0.12204801647484315, y: 0.09078590793152891 },
-    // ], // Need to figure out how to crop
+    scale: { from: { unit: "IN", value: 0.125 }, to: { unit: "FT", value: 1 } }, // used for recalibrating
+    croppath: [
+      // Need to figure out how to crop. Maybe gdalwarp?
+      { x: 0.12204801647484315, y: 0.09078590793152891 },
+      { x: 0.060104541602532514, y: 0.31571815727373986 },
+      { x: 0.272067369787775, y: 0.6422764228605643 },
+      { x: 0.7898761052951476, y: 0.6436314364059776 },
+      { x: 0.8527874469953014, y: 0.44579945808058374 },
+      { x: 0.6592140878979672, y: 0.10840108409642901 },
+      { x: 0.12204801647484315, y: 0.09078590793152891 },
+    ],
     corners: {
       topleft: { longitude: -96.79964987948574, latitude: 32.80371614021954 },
       topright: { longitude: -96.79855632413677, latitude: 32.80377452931311 },
@@ -33,8 +34,8 @@ const BeginMapTilingProcessRequest = {
         latitude: 32.80311798118891,
       },
     },
-    // bearing: 3.6347673397717295, // used for recalibrating
-    // centerpoint: { longitude: -96.7990782923411, latitude: 32.803417061898756 }, // used for recalibrating
+    bearing: 3.6347673397717295, // used for recalibrating
+    centerpoint: { longitude: -96.7990782923411, latitude: 32.803417061898756 }, // used for recalibrating
   },
 };
 
